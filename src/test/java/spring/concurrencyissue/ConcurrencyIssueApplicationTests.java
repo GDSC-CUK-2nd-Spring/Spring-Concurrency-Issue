fd7e8dd0476cfac903996bcc68564cef56745ec1
sor999 <sorsor999@naver.com>
2024-03-31 19:26:39 +0900

    feat: 프로젝트 초기 설정

---
+++ src/test/java/spring/concurrencyissue/ConcurrencyIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.concurrencyissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
